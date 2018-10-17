fd8e6ff193176e4e5b497a032fd2bd1d8c12d3e9
Magnús Óli Guðmundsson <magnusog15@ru.is>
2018-10-17 18:08:35 +0000

    final

--- reduxStore.js
+++ reduxStore.js
@@ -2,26 +2,32 @@ import { createStore } from "redux";
 
 const initialState = [];
 
+updateList = (state, action) => {
+  return [...state, { name: action.payload.name, done: action.payload.done }];
+};
+deleteList = (state, action) => {
+  return state.filter(obj => {
+    if (obj.name != action.payload.name) {
+      return obj;
+    }
+  });
+};
+switchList = (state, action) => {
+  return state.map(obj => {
+    if (obj.name === action.payload.name) {
+      obj.done = !action.payload.done;
+    }
+    return obj;
+  });
+};
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case "UPDATE_LIST":
-      return [
-        ...state,
-        { name: action.payload.name, done: action.payload.done }
-      ];
+      return this.updateList(state, action);
     case "UPDATE_DELETE":
-      return state.filter(obj => {
-        if (obj.name != action.payload.name) {
-          return obj;
-        }
-      });
+      return this.deleteList(state, action);
     case "UPDATE_SWITCH":
-      return state.map(obj => {
-        if (obj.name === action.payload.name) {
-          obj.done = !action.payload.done;
-        }
-        return obj;
-      });
+      return this.switchList(state, action);
     default:
       return state;
   }
